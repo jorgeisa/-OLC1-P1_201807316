c0f413c59ff73655782ff46cf6705e35c28eead5
Isaac Xicol <61366461+jorgeisa@users.noreply.github.com>
2020-09-14 06:16:13 -0600

    Reportes JS

--- P1/AnalizadorLexicoJS.py
+++ P1/AnalizadorLexicoJS.py
@@ -1,3 +1,5 @@
+from graphviz import Digraph
+
 from Token import TipoToken
 from Token import Token
 from ErrorLexico import ErrorLexico
@@ -17,6 +19,13 @@ class AnalizadorLexicoJS:
         self.contadorV = 1
         self.contadorH = 1
         self.posicion = 0
+        self.dotAFD1 = Digraph(comment="Grafica1", format='jpg')
+        self.dotAFD2 = Digraph(comment="Grafica2", format='jpg')
+        self.dotAFD3 = Digraph(comment="Grafica3", format='jpg')
+        self.boolGrafo1 = False
+        self.boolGrafo2 = False
+        self.boolReservada = False
+        self.boolGrafo3 = False
 
     def agregarToken(self, tipoToken, lexemaValor):
         self.lista_Tokens.append(Token(tipoToken, lexemaValor))
@@ -47,6 +56,8 @@ class AnalizadorLexicoJS:
                 caracterActual = self.entradaTexto[self.posicion]
 
             # E0 -> E Simbolos "Especiales"
+            # E0-> E13 , E14, E15, E16, E17, E18, E19, E22
+            # E22, E23, E24, E25, E26, E27, E29, E31, E32
             elif caracterActual == "+":
                 self.estadoE13()
             elif caracterActual == "=":
@@ -68,19 +79,22 @@ class AnalizadorLexicoJS:
             elif caracterActual.isnumeric():  # NUMERO ENTERO
                 self.estadoE1()
 
-            # E0 -> E6
+            # E0 -> E6, E2,
             elif caracterActual.isalpha():  # ID | RESERVADAS
                 finalActual = self.obtenerLongitud() + self.posicion
                 self.estadoE6()
                 self.posicion = finalActual  # poner fuera del metodo
+                if not self.boolGrafo2:
+                    self.boolGrafo2 = True
+                    self.dotAFD2.render(filename="AFD 2")
 
-            # E0 -> E8
+            # E0 -> E8, E10, E11, E12, E9
             elif caracterActual == "/":  # comentario , /
                 self.estadoE8()
-            # E0 -> E cadenas
+            # E0 -> E20, E28 cadenas
             elif caracterActual == "'":
                 self.estadoE20()
-            # E0 -> E cadenas
+            # E0 -> E21, EE30 cadenas
             elif caracterActual == "\"":
                 self.estadoE21()
             # E0 -> E0
@@ -135,12 +149,22 @@ class AnalizadorLexicoJS:
     # ----------------------     PALABRAS RESERVADAS   ----------------------
     # E0 -> E6 ("/")
     def estadoE6(self):  # letra | ID | RESERVADAS
+        if not self.boolGrafo2:
+            self.dotAFD2.attr(rankdir='LR')
+            self.dotAFD2.node("0", label="E0", shape="circle")
+            self.dotAFD2.node("2", label="E2", shape="doublecircle")
+
         final = self.obtenerLongitud() + self.posicion
         for i in range(self.posicion, final):
             self.lexemaTemp += self.entradaTexto[i]
 
         # E0 -> E6
         if self.evaluarReservadas():
+            if not self.boolGrafo2:
+                self.dotAFD2.edge("0", "2", self.entradaTexto[self.posicion])
+                for i in range(self.posicion, final):
+                    if (self.posicion + 1) != final:
+                        self.dotAFD2.edge("2", "2", self.entradaTexto[i + 1], dir="forward")
             self.contadorH += self.obtenerLongitud()
             return
 
@@ -150,9 +174,12 @@ class AnalizadorLexicoJS:
     # E2 -> E2
     def estadoE2(self, final):
         self.lexemaTemp = ""  # 40,41,42,43,44,45,46,  47       ,48,49,50
+
+        if not self.boolGrafo2:
+            self.dotAFD2.edge("0", "2", self.entradaTexto[self.posicion])
+
         while self.posicion < final:  # id4@3id  0 - 6
             caracter = self.entradaTexto[self.posicion]
-
             if caracter.isalpha():
                 self.lexemaTemp += caracter
             elif caracter.isnumeric():
@@ -166,6 +193,15 @@ class AnalizadorLexicoJS:
             if (self.posicion + 1) == final:
                 if not self.evaluarReservadas():
                     self.agregarToken(TipoToken.ID, self.lexemaTemp)
+
+            if not self.boolGrafo2:
+                if self.entradaTexto[self.posicion + 1].isalpha() and (self.posicion + 1) != final:
+                    self.dotAFD2.edge("2", "2", self.entradaTexto[self.posicion + 1])
+                elif self.entradaTexto[self.posicion + 1].isnumeric() and (self.posicion + 1) != final:
+                    self.dotAFD2.edge("2", "2", self.entradaTexto[self.posicion + 1])
+                elif self.entradaTexto[self.posicion + 1] == "_" and (self.posicion + 1) != final:
+                    self.dotAFD2.edge("2", "2", self.entradaTexto[self.posicion + 1])
+
             self.posicion += 1
             self.contadorH += 1
 
@@ -179,6 +215,11 @@ class AnalizadorLexicoJS:
             # Para el path del comentario
             if self.contadorComentario != 3:
                 self.contadorComentario += 1
+
+            # if self.boolGrafo3:
+            #     self.dotAFD3.node("9", label="E9", shape="circle")
+            #     self.dotAFD3.edge("8", "9", f"{caracter}")
+
             self.lexemaTemp += caracter
             self.posicion += 1
             self.contadorH += 1
@@ -186,6 +227,15 @@ class AnalizadorLexicoJS:
             self.estadoE9()
             return
         elif caracter == "*":
+            if not self.boolGrafo3:
+                self.dotAFD3.attr(rankdir="LR")
+                self.dotAFD3.node("0", label="E0", shape="circle")
+                self.dotAFD3.node("8", label="E8", shape="circle")
+                self.dotAFD3.edge("0", "8", f"{self.entradaTexto[self.posicion-1]}")
+
+                self.dotAFD3.node("10", label="E10", shape="circle")
+                self.dotAFD3.edge("8", "10", f"{caracter}")
+
             self.lexemaTemp += caracter
             self.posicion += 1
             self.contadorH += 1
@@ -207,6 +257,8 @@ class AnalizadorLexicoJS:
                 self.agregarToken(TipoToken.COMENTARIO_UNILINEA, self.lexemaTemp)
                 return
             else:
+                # if self.boolGrafo3:
+                #     self.dotAFD3.edge("9", "9", f"{caracter}")
                 self.lexemaTemp += caracter
             # Recuperar Path de Salida
             if caracter == "C" and self.contadorComentario == 2:
@@ -225,16 +277,30 @@ class AnalizadorLexicoJS:
             caracter = self.entradaTexto[self.posicion]
             if caracter == "*":
                 # E11
+                if not self.boolGrafo3:
+                    self.dotAFD3.node("11", label="E11", shape="circle")
+                    self.dotAFD3.edge("10", "11", f"{caracter}")
+
                 self.lexemaTemp += caracter
                 if self.entradaTexto[self.posicion + 1] == "/":
                     # E11-> E12
+                    if not self.boolGrafo3:
+                        self.dotAFD3.node("12", label="E12", shape="doublecircle")
+                        self.dotAFD3.edge("11", "12", f"{self.entradaTexto[self.posicion + 1]}")
+
                     self.lexemaTemp += self.entradaTexto[self.posicion + 1]
                     self.posicion += 2
                     self.contadorH += 2
                     self.agregarToken(TipoToken.COMENTARIO_MULTILINEA, self.lexemaTemp)
+                    if not self.boolGrafo3:
+                        self.boolGrafo3 = True
+                        self.dotAFD3.render(filename="AFD 3")
                     return
                 # E11 -> E10
             else:
+                if not self.boolGrafo3:
+                    self.dotAFD3.edge("10", "10", f"{caracter}")
+
                 self.lexemaTemp += caracter
                 if caracter == "\n":
                     self.contadorV += 1
@@ -387,6 +453,11 @@ class AnalizadorLexicoJS:
 
     # E0 -> E21 (")
     def estadoE21(self):
+        if not self.boolGrafo1:
+            self.dotAFD1.attr(rankdir='LR')
+            self.dotAFD1.node(f"0", label="E0", shape="circle")
+            self.dotAFD1.node(f"21", label="E21", shape="circle", width="1")
+            self.dotAFD1.edge("0", "21", "\" Comillas")
         self.lexemaTemp += self.entradaTexto[self.posicion]
         self.posicion += 1
         self.contadorH += 1
@@ -396,12 +467,23 @@ class AnalizadorLexicoJS:
             self.lexemaTemp += caracter
             if caracter == "\"":
                 # E21 -> E30
+                if not self.boolGrafo1:
+                    self.boolGrafo1 = True
+                    self.dotAFD1.node("30", label='E30', shape="doublecircle")
+                    self.dotAFD1.edge("21", "30", "\" Comillas")
+                    self.dotAFD1.render(filename="AFD 1")
                 self.agregarToken(TipoToken.CADENA_DOBLES, self.lexemaTemp)
                 self.posicion += 1
                 self.contadorH += 1
                 return
+            self.dotAFD1.edge("21", "21", f"{caracter}", dir='forward')
             self.contadorH += 1
             self.posicion += 1
+        if not self.boolGrafo1:
+            self.boolGrafo1 = True
+            self.dotAFD1.node("30", label='E30', shape="doublecircle")
+            self.dotAFD1.edge("21", "30", "Fin")
+            self.dotAFD1.render(filename="AFD 1")
         self.agregarToken(TipoToken.CADENA_DOBLES, self.lexemaTemp)
 
     def estadoE22(self):
@@ -454,33 +536,63 @@ class AnalizadorLexicoJS:
     def evaluarReservadas(self):
         if self.lexemaTemp.lower() == "return":
             self.agregarToken(TipoToken.RESERVADA_RETURN, "return")
+            if self.boolGrafo2:
+                self.dotAFD2.node("6", label="E6", shape="doublecircle")
+                self.dotAFD2.edge("0", "6", "return")
             return True
         elif self.lexemaTemp.lower() == "break":
             self.agregarToken(TipoToken.RESERVADA_BREAK, "break")
+            if self.boolGrafo2:
+                self.dotAFD2.node("6", label="E6", shape="doublecircle")
+                self.dotAFD2.edge("0", "6", "break")
             return True
         elif self.lexemaTemp.lower() == "continue":
             self.agregarToken(TipoToken.RESERVADA_CONTINUE, "continue")
+            if self.boolGrafo2:
+                self.dotAFD2.node("6", label="E6", shape="doublecircle")
+                self.dotAFD2.edge("0", "6", "continue")
             return True
         elif self.lexemaTemp.lower() == "do":
             self.agregarToken(TipoToken.RESERVADA_DO, "do")
+            if self.boolGrafo2:
+                self.dotAFD2.node("6", label="E6", shape="doublecircle")
+                self.dotAFD2.edge("0", "6", "do")
             return True
         elif self.lexemaTemp.lower() == "while":
             self.agregarToken(TipoToken.RESERVADA_WHILE, "while")
+            if self.boolGrafo2:
+                self.dotAFD2.node("6", label="E6", shape="doublecircle")
+                self.dotAFD2.edge("0", "6", "while")
             return True
         elif self.lexemaTemp.lower() == "for":
             self.agregarToken(TipoToken.RESERVADA_FOR, "for")
+            if self.boolGrafo2:
+                self.dotAFD2.node("6", label="E6", shape="doublecircle")
+                self.dotAFD2.edge("0", "6", "for")
             return True
         elif self.lexemaTemp.lower() == "if":
             self.agregarToken(TipoToken.RESERVADA_IF, "if")
+            if self.boolGrafo2:
+                self.dotAFD2.node("6", label="E6", shape="doublecircle")
+                self.dotAFD2.edge("0", "6", "if")
             return True
         elif self.lexemaTemp.lower() == "var":
             self.agregarToken(TipoToken.RESERVADA_VAR, "var")
+            if self.boolGrafo2:
+                self.dotAFD2.node("6", label="E6", shape="doublecircle")
+                self.dotAFD2.edge("0", "6", "var")
             return True
         elif self.lexemaTemp.lower() == "math":
             self.agregarToken(TipoToken.RESERVADA_VAR, "Math")
+            if self.boolGrafo2:
+                self.dotAFD2.node("6", label="E6", shape="doublecircle")
+                self.dotAFD2.edge("0", "6", "Math")
             return True
         elif self.lexemaTemp.lower() == "pow":
             self.agregarToken(TipoToken.RESERVADA_VAR, "pow")
+            if self.boolGrafo2:
+                self.dotAFD2.node("6", label="E6", shape="doublecircle")
+                self.dotAFD2.edge("0", "6", "pow")
             return True
         return False
 
--- P1/main.py
+++ P1/main.py
@@ -1,14 +1,32 @@
+from graphviz import Digraph
+
 from PantallaPrincipal import PantallaPrincipal
 import os
 
-
-def crearArchivo():
-    # os.linesep para separar una linea de otra
-    file = open("C:/Users/Isaac/Desktop/nombrexd.txt", "w")
-    file.write("Primera línea\n")
-    file.write("Segunda línea")
-    file.close()
-
-
-# crearArchivo()
 iniciar = PantallaPrincipal()
+
+# Para el grafo de JS se tomo en cuenta lo siguiente
+# Grafo 1 = Primera Cadena
+# Grafo 2 = Primer ID
+# Grafo 3 = Primer Comentario MULTILINEA
+
+
+# Ejemplo pagina
+# dot = Digraph(comment='The Round Table', format='jpg')
+# dot.node('A', 'King Arthur')
+# dot.node('B', 'Sir Bedevere')
+# dot.node('L', 'Sir Lancelot')
+#
+# dot.edges(['AB', 'AL'])
+# dot.edge('B', 'L', constraint='false')
+# dot.render(filename="dot")
+
+# Realizado
+# dotArbol = Digraph(comment=f"nombre1Arbol", format='jpg')
+# dotArbol.attr(rankdir='LR')
+# dotArbol.node(f"1", label=f"jsjs", shape="circle")
+# dotArbol.node(f"2", label=f"juju", shape="doublecircle", width="2")
+# dotArbol.edge(f"1", "1", "enlace", dir="forward",)
+# dotArbol.edge(f"1", "1", "Hola", dir="forward")
+# dotArbol.edge(f"1", "2", "j")
+# dotArbol.render(filename=f"nombreArbolxd")
